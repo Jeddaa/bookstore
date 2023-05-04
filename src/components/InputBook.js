f1d5764bf117a5a3218b40e9b6e90b84de4c5dd4
jeddaa <fathiato@gmail.com>
2023-05-04 14:22:17 +0100

    styling the input book

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,13 +1,13 @@
 const InputBook = () => (
-  <div>
-    <form>
+  <div className="input-book-div">
+    <form className="input-book-form">
       <label htmlFor="test">
-        ADD NEW BOOK
+        <h2>ADD NEW BOOK</h2>
         <input type="text" placeholder="Book title" />
         <input type="text" placeholder="Author" />
       </label>
     </form>
-    <button type="submit">ADD BOOK</button>
+    <button className="add-book-btn" type="submit">ADD BOOK</button>
   </div>
 );
 
