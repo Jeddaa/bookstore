e6eddc5d2df4cb84220e51730469dbeb8a298011
jeddaa <fathiato@gmail.com>
2023-05-11 03:58:27 +0100

    get books from external api

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,22 +1,33 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/bookSlice';
+import { removeBook, deleteBook } from '../redux/books/bookSlice';
 
-const Book = ({ title, author, id }) => {
+const Book = ({
+  title, author, id, category,
+}) => {
   const dispatch = useDispatch();
+  const handleDelete = (id) => {
+    dispatch(removeBook(id));
+    dispatch(deleteBook(id));
+  };
+
   return (
     <li>
       <div>
         <h2>{title}</h2>
         <p>{author}</p>
-        <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
+        <p>{category}</p>
+        <button type="button" onClick={() => handleDelete(id)}>
+          Remove
+        </button>
       </div>
     </li>
   );
 };
-export default Book;
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
+export default Book;
--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,36 +1,63 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
-import { addBook } from '../redux/books/bookSlice';
+import { addBook, postBook } from '../redux/books/bookSlice';
 
 const InputBook = () => {
   const dispatch = useDispatch();
-  const [values, setValues] = useState({});
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setValues((values) => ({
-      ...values, id: crypto.randomUUID(), [name]: value,
-    }));
-  };
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [author, setAuthor] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!values.title || !values.author) return;
-    dispatch(addBook(values));
-    setValues({});
+    if (!title || !author || !category) setError('Please fill out all fields');
+    else {
+      dispatch(
+        postBook({
+          item_id: crypto.randomUUID(),
+          title,
+          author,
+          category,
+        }),
+      );
+      dispatch(
+        addBook({
+          item_id: crypto.randomUUID(),
+          title,
+          author,
+          category,
+        }),
+      );
+      setError('');
+      setTitle('');
+      setAuthor('');
+      setCategory('');
+    }
   };
   return (
     <div>
       <form>
         <label htmlFor="test">
           ADD NEW BOOK
-          <input type="text" name="title" placeholder="Book title" value={values.title || ''} onChange={handleChange} />
-          <input type="text" name="author" placeholder="Author" value={values.author || ''} onChange={handleChange} />
+          <input type="text" name="title" placeholder="Book title" value={title || ''} onChange={(e) => setTitle(e.target.value)} />
+          <input type="text" name="author" placeholder="Author" value={author || ''} onChange={(e) => setAuthor(e.target.value)} />
+          <select type="text" name="category" value={category} onChange={(e) => setCategory(e.target.value)} required>
+            <option value="" disabled selected>
+              select Category
+            </option>
+            <option>Fiction</option>
+            <option>Horror</option>
+            <option>Romance</option>
+            <option>Science</option>
+            <option>NonFiction</option>
+          </select>
         </label>
       </form>
       <button type="submit" onClick={handleSubmit}>
         ADD BOOK
       </button>
+      <span className="error">{error}</span>
     </div>
   );
 };
