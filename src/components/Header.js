aadc8f2dcf0153cd3b77563f44fd9aebe63f9748
jeddaa <fathiato@gmail.com>
2023-05-12 13:12:40 +0100

    making review changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,26 @@
 import { Link } from 'react-router-dom';
+import { BsFillPersonFill } from 'react-icons/bs';
 
 const Header = () => (
   <nav className="navbar">
-    <h1 className="nav-header">Bookstore CMS</h1>
-    <ul>
-      <li>
-        <Link className="nav-link" to="/">
-          BOOKS
-        </Link>
-      </li>
-      <li>
-        <Link className="nav-link" to="/categories">
-          CATEGORIES
-        </Link>
-      </li>
-    </ul>
+    <div className="navbar-div">
+      <h1 className="nav-header">Bookstore CMS</h1>
+      <ul>
+        <li>
+          <Link className="nav-link" to="/">
+            BOOKS
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/categories">
+            CATEGORIES
+          </Link>
+        </li>
+      </ul>
+    </div>
+    <button type="button" className="user-icon-btn">
+      <BsFillPersonFill className="user-icon" />
+    </button>
   </nav>
 );
 
